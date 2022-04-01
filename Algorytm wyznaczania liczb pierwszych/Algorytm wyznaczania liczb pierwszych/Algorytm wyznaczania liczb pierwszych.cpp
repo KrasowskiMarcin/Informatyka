bcd556a74b6d3e8b25b09e512a2c35ec189b2cd6
Marcin Krasowski 3A <mkrasowski146@technikumkreatywne.pl>
2022-04-01 10:09:50 +0200

    Algorytm wyznaczania liczb pierwszych

---
+++ Algorytm wyznaczania liczb pierwszych/Algorytm wyznaczania liczb pierwszych/Algorytm wyznaczania liczb pierwszych.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	// input od użytkownika
+	int n;
+	cout << "Wprowadź N: ";
+	cin >> n;
+
+	// zmienne
+	int lp = 0;
+	int p = 2;
+	int d;
+
+
+
+	while (lp < n) {
+		bool isPrime = true;
+		for (int d = 2; d <= p - 1; d++) {
+			if (p % d == 0) {
+				isPrime = false;
+			}
+		}
+
+		if (isPrime) {
+			cout << p << endl;
+			lp++;
+		}
+		p++;
+	}
+}
