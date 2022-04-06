1d3a66b15707f260244fa0c152c4d236d8f48034
Marcin Krasowski 3A <mkrasowski146@technikumkreatywne.pl>
2022-04-06 10:51:34 +0200

    Algorytm obliczający NWW i NWD

---
+++ Obliczanie NWW i NWD/Obliczanie NWW i NWD/Obliczanie NWW i NWD.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    // input
+    cout << "Podaj pierwszą liczbę: ";
+    cin >> a;
+    cout << "Podaj drugą liczbę: ";
+    cin >> b;
+
+    int iloczyn = (a * b);
+    int nww;
+
+    while (a != b) {
+        if (a < b) {
+            b -= a;
+        }
+        else {
+            a -= b;
+        }
+    }
+
+    nww = iloczyn / a;
+
+    // output
+    cout << "Największy wspólny dzielnik: " << a << endl;
+    cout << "Największa wspóna wartość: " << nww;
+}
