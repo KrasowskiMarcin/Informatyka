c2125ec1b59d03d3bc1e23b37888d90561a9a185
Marcin Krasowski 3A <mkrasowski146@technikumkreatywne.pl>
2022-04-22 09:57:59 +0200

    Algorytm ciągu Fibonacciego

---
+++ Ciąg Fibonacciego/Ciąg Fibonacciego/Ciąg Fibonacciego.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+void fibonacci(int n) {
+	int f0 = 0;
+	int f1 = 1;
+	int f;
+
+	for (int i = 0; i <= n; i++) {
+		if (i > 1) {
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else {
+			f = i;
+		}
+		cout << f<<endl;
+	}
+}
+
+int main()
+{
+	int liczba;
+	cout << "podaj n: ";
+	cin >> liczba;
+
+	fibonacci(liczba);
+}
